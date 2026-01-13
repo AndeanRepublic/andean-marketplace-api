0772a61138dedfe7280be91d914d23da33cda30e
Anghelo Alagon <200518@unsaac.edu.pe>
2026-01-13 18:09:47 -0500

    feat: add MediaItem entity and repository interface

---
+++ src/andean/app/datastore/MediaItem.repo.ts
@@ -0,0 +1,13 @@
+import { MediaItem } from '../../domain/entities/MediaItem';
+
+export abstract class MediaItemRepository {
+  abstract getById(id: string): Promise<MediaItem | null>;
+  
+  abstract getAll(): Promise<MediaItem[]>;
+  
+  abstract save(mediaItem: MediaItem): Promise<MediaItem>;
+  
+  abstract update(mediaItem: MediaItem): Promise<MediaItem>;
+  
+  abstract delete(id: string): Promise<void>;
+}
