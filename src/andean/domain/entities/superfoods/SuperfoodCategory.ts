4bcc2aa20805653802a7da9b2b6f2f3ec58c8c20
Anghelo Alagon <200518@unsaac.edu.pe>
2026-01-13 18:00:32 -0500

    feat: add Superfood entities including Benefit, Category, Certification, PreservationMethod, ProductPresentation, and SalesUnitSize

---
+++ src/andean/domain/entities/superfoods/SuperfoodCategory.ts
@@ -0,0 +1,9 @@
+export class SuperfoodCategory {
+	constructor(
+		public readonly id: string,
+		public readonly name: string,
+		public readonly status: 'ENABLED' | 'DISABLED',
+		public readonly createdAt?: Date,
+		public readonly updatedAt?: Date,
+	) { }
+}
