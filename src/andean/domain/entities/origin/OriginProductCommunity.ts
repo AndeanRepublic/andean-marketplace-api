bb228a52a61a72d4f8dfff829a389d7f3769d723
Anghelo Alagon <200518@unsaac.edu.pe>
2026-01-13 22:45:20 -0500

    feat: create OriginProductCommunity and OriginProductRegion entities

---
+++ src/andean/domain/entities/origin/OriginProductCommunity.ts
@@ -0,0 +1,9 @@
+export class OriginProductCommunity {
+	constructor(
+		public id: string,
+		public name: string,
+		public regionId: string,
+		public createdAt: Date,
+		public updatedAt: Date,
+	) { }
+}
