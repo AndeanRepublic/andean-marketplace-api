2983d1708622de4c79d596adfc25de448ad7eaea
Anghelo Alagon <200518@unsaac.edu.pe>
2026-02-12 10:38:39 -0500

    feat: refactor UpdateReviewUseCase and UpdateReviewDto to remove mediaId validation and simplify structure

--- src/andean/infra/controllers/dto/UpdateReviewDto.ts
+++ src/andean/infra/controllers/dto/UpdateReviewDto.ts
@@ -1,4 +1,29 @@
-import { CreateReviewDto } from './CreateReviewDto';
-import { PartialType } from '@nestjs/swagger';
+import {
+	IsString,
+	IsInt,
+	IsEnum,
+	IsOptional,
+} from 'class-validator';
+import { ProductType } from 'src/andean/domain/enums/ProductType';
 
-export class UpdateReviewDto extends PartialType(CreateReviewDto) {}
+export class UpdateReviewDto {
+	@IsString()
+	@IsOptional()
+	content?: string;
+
+	@IsInt()
+	@IsOptional()
+	numberStarts?: number;
+
+	@IsString()
+	@IsOptional()
+	customerId?: string;
+
+	@IsString()
+	@IsOptional()
+	productId?: string;
+
+	@IsEnum(ProductType)
+	@IsOptional()
+	productType?: ProductType;
+}
