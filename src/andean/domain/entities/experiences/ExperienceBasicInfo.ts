f4892725800fdab872cd6f3f9c5d2f19cab56d66
Anghelo Alagon <200518@unsaac.edu.pe>
2026-02-17 18:48:26 -0500

    feat: implement database schemas for various experience-related entities

---
+++ src/andean/domain/entities/experiences/ExperienceBasicInfo.ts
@@ -0,0 +1,17 @@
+import { ExperienceLanguage } from '../../enums/ExperienceLanguage';
+import { OwnerType } from '../../enums/OwnerType';
+
+export class ExperienceBasicInfo {
+	constructor(
+		public id: string,
+		public title: string,
+		public ubication: string,
+		public days: number,
+		public nights: number,
+		public minNumberGroup: number,
+		public maxNumberGroup: number,
+		public languages: ExperienceLanguage[],
+		public ownerType: OwnerType,
+		public ownerId: string,
+	) { }
+}
